33ac7391c0d824c830d6ce7e60993b0d36fd65d9
Ernest Kamau <1.kamauernest@gmail.com>
2025-03-14 09:00:00 +0300

    Finished codegrade

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
+]
+
+
+function superbowlWin(arrayOfObjects){
+    let data = arrayOfObjects.find(win => win.result === "W");
+
+    return data ? data.year : undefined;
+}
+
+console.log(superbowlWin(record));
